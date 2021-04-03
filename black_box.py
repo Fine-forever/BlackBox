181e9a0111f8b8c90a611bae4c80e581e5ac2289
Fine-forever <74816479+Fine-forever@users.noreply.github.com>
2021-04-03 16:22:51 +0600

    Add files via upload

--- black_box.py
+++ black_box.py
@@ -3,14 +3,15 @@ from bs4 import BeautifulSoup
 class black_box:
     _instance = None
     def __init__(self):
-        self.sys = {'Aa' : 'Алпресе Авиягро'}#соотношения имён и сочетаний букв
+        self.sys = {'Aa':'Алпресе Авиягро','Ab':'Антор Брукало','Ac':'Амер Салага'}#соотношения имён и сочетаний букв
         self.code = 'Никола Тесла'
-        self.r = requests.get('translate.google.com')
+        self.r = requests.get('https://translate.google.com/')
         self.soup = BeautifulSoup(self.r.text, 'lxml')
     def give_answer(self, x):
         self.r.params = '?sl=ru&tl=en&text='+x+'&op=translate'
-        m = self.soup.find_all('c-wiz', class_='text')
+        m = self.soup.find_all('c-wiz', class_='P6w8m BDJ8fb')
         #будет возвращать перевод, брать буквы и сверять со словарём
+        print(m)
 
     @classmethod
     def get_instance(cls):
@@ -18,4 +19,5 @@ class black_box:
             cls._instance = black_box()
         return cls._instance
 
-
+i = black_box().get_instance()
+i.give_answer('электричество')
